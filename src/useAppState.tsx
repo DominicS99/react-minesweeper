0179934aea5513ffa287a8393d2c01653e67572b
Dominic S <dominic.sagen@gmail.com>
2025-06-05 17:55:26 -0700

    Added reveal-cell action as well as buttons to get to each phase. Please note that the board will not re-generate on post-game yet.

--- src/useAppState.tsx
+++ src/useAppState.tsx
@@ -22,6 +22,11 @@ export type Action =
   | {
       type: "load-board";
       board: Board;
+    }
+  | {
+      type: "reveal-cell";
+      row: number;
+      col: number;
     };
 
 function reducer(state: State, action: Action): State {
@@ -38,6 +43,47 @@ function reducer(state: State, action: Action): State {
         ...state,
         board: action.board,
       };
+    case "reveal-cell":
+      if (state.phase !== "in-game" || !state.board) return state; // Confirm phase and board exists
+      const { row, col } = action;
+      if (state.board.display[row][col] !== -1) return state; // Do nothing if already checked
+
+      if (state.board.mines[row][col]) {
+        // Game over
+        return {
+          ...state,
+          phase: "post-game",
+          board: state.board,
+        };
+      }
+
+      const mineCount = state.board.mines.reduce(
+        (count, r, rIdx) =>
+          count +
+          r.reduce(
+            (cCount, cell, cIdx) =>
+              cCount +
+              (cell && Math.abs(rIdx - row) <= 1 && Math.abs(cIdx - col) <= 1
+                ? 1
+                : 0),
+            0,
+          ),
+        0,
+      );
+
+      const newDisplay = state.board.display.map((r, rIdx) =>
+        rIdx === row
+          ? r.map((cell, cIdx) => (cIdx === col ? mineCount : cell))
+          : r.slice(),
+      );
+      return {
+        ...state,
+        board: {
+          ...state.board,
+          display: newDisplay,
+        },
+      };
+
     default:
       throw new Error(`Unknown action: ${action}`);
   }
